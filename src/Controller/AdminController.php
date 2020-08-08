ef6fdd7f19373dd13f1eb6f01860380a959b9ffd
Tommy Rooryck <tommy_rooryck@hotmail.com>
2020-08-08 15:26:15 +0200

    23.4

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -9,10 +9,16 @@ use phpDocumentor\Reflection\Types\This;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\HttpCache\HttpCache;
+use Symfony\Component\HttpKernel\KernelInterface;
 use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Workflow\Registry;
 use Twig\Environment;
+
+/**
+ * @Route("/admin")
+ */
 
 class AdminController extends AbstractController
 {
@@ -32,7 +38,7 @@ class AdminController extends AbstractController
     }
 
     /**
-     * @Route("/admin/comment/review/{id}", name="review_comment")
+     * @Route("/comment/review/{id}", name="review_comment")
      * @param Request $request
      * @param Comment $comment
      */
@@ -61,6 +67,20 @@ class AdminController extends AbstractController
          'transition' => $transition,
           'comment' => $comment,
       ]);
-
   }
+
+//  /**
+//   * @Route("/http-cache/{uri<.*>}", methods={"PURGE"})
+//   */
+//  public function purgeHttpCache(KernelInterface $kernel, Request $request, string $uri)
+//  {
+//      if ('prod' === $kernel->getEnvironment()){
+//          return new Response('KO', 400);
+//      }
+//
+//      $store = (new class($kernel) extends HttpCache {})->getStore();
+//      $store->purge($request->getSchemeAndHttpHost(). '/'.$uri);
+//
+//      return new Response('Done');
+//  }
 }
